1382ba67a4ccf309f140e2f5453454aecacbfe37
parth-simform-007 <parth.bhavsar@simformsolutions.com>
2025-02-25 13:29:27 +0530

    Created Demo For DI for Nest APP

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { MessageModule } from './messages/message.module';
+import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessageModule);
+  const app = await NestFactory.create(ComputerModule);
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/messages/message.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsDefined, IsString } from "class-validator";
-
-export class createMessageDTO {
-    @IsDefined()
-    @IsString()
-    content: string
-}
